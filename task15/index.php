4421291055b2a2691411b8a5ccc6552df36be1f6
Al Fatih Muzakki <malfazakki@gmail.com>
2025-04-06 10:51:08 +0700

    feat: finished task15

---
+++ task15/index.php
@@ -0,0 +1,24 @@
+<?php
+function generateFibonacci($n) {
+    $fibonacci = [];
+    if ($n >= 1) {
+        $fibonacci[] = 0;
+    }
+    if ($n >= 2) {
+        $fibonacci[] = 1;
+    }
+    
+    for ($i = 2; $i < $n; $i++) {
+        $fibonacci[] = $fibonacci[$i-1] + $fibonacci[$i-2];
+    }
+    
+    return $fibonacci;
+}
+
+// Input: Generate first 10 Fibonacci numbers
+$input = 10;
+$fibSequence = generateFibonacci($input);
+
+// Output the result
+echo implode(',', $fibSequence);
+?>
